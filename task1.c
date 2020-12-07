1281d132133be1d39b1e6a58a688feb6eec538b2
Uberariy <70648019+Uberariy@users.noreply.github.com>
2020-12-07 19:34:35 +0300

    Create task1.c

---
+++ task1.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    
+
+
+    return(0);
+}
