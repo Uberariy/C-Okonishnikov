cfcfa9ce96e1fa7b2c24908afbbc2fdf33fbf588
Uberariy <70648019+Uberariy@users.noreply.github.com>
2020-12-08 16:31:29 +0300

    ПОЧТИ ГОТОВО

--- task2.c
+++ task2.c
@@ -18,28 +18,35 @@ int main(int argc, char *argv[])
     if (argc<2)
     {   fprintf(stderr, "Error!\n");    return(1);   }
 
-    N = atoi(argv[1]);
+    N = atoi(argv[1]);  //fprintf(stderr, "%d", N);
     sargv = (char **)malloc((N+2)*(sizeof(char *)));
     sargv[0] = (char *)malloc((6)*(sizeof(char)));
     strcpy(sargv[0], "echo");
 
-    for (i = 1; i++; i<=N+1)
+    for (i = 1; i<=N+1; i++)
     {
-        sargv[i] = (char *)malloc((2)*(sizeof(char)));
+        sargv[i] = (char *)malloc(sizeof(char)*2);
     }
-    for (i = 1; i++; i<=N)
+    for (i = 1; i<=N; i++)
     {
-        sargv[i][0] = i;
+        sargv[i][0] = (char)i;
         sargv[i][1] = '\0';
     }
-    //sargv[N+1] = NULL;
+    sargv[N+1] = NULL;
 
+    if (fork() == 0)
+    {
+        execvp(sargv[0], sargv);
+        perror("exec!!");
+        exit(1);
+    }
+    else (wait(&stat));
 
-    free(sargv);
-    for (i = 0; i++; i<=N+1)
+    for (i = 0; i<=N+1; i++)
     {
         free(sargv[i]);
     }
+    free(sargv);
 
     return(0);
 }
