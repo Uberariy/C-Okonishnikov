57a15cf6d078fe8f25abc2dd411b583bf5abc373
Uberariy <70648019+Uberariy@users.noreply.github.com>
2020-12-08 15:38:46 +0300

    inter(not ready)
    inter(not ready)

--- task2.c
+++ task2.c
@@ -11,19 +11,41 @@
 int main(int argc, char *argv[])
 {
     pid_t pid, p;
-    int i, j, fd1, stat, c;
+    int i, j, fd1, fd2, stat, numb=0;
+    char c;
     int fd[2];
 
     if (argc<2)
     {   fprintf(stderr, "error");   return(1);  }
 
-    fd1 = open(argv[1], O_RDWR, 0777);
+    fd1 = open(argv[1], O_RDONLY, 0777);
+    fd2 = open(argv[1], O_RDWR, 0777);
     int fsize=0;
+    int fsize2=0;
     while (read(fd1, &c, 1))
     {
         fsize++;
-        write(fd1, &c, 1);
+        write(fd2, &c, 1);
+        if (c == '\n') 
+        {
+            fsize2 = fsize;
+        }
+
+        if ((c >= '0') && (c <= '9'))
+            numb++;
+
+        write(fd2, &c, 1);
+        if (numb==2)
+        {
+            while ((read(fd1, &c, 1)) && (c != '\n'))
+            {
+                write(fd2, &c, 1);
+            }
+            lseek(fd2, fsize2, SEEK_SET);
+        }
+        write(fd2, &c, 1);
     }
+    ftruncate(fd2, fsize);
 
     return(0);
 }
