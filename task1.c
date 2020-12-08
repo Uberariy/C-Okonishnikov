7cf8a18925ef5fda33831a28b04fee53e416d972
Uberariy <70648019+Uberariy@users.noreply.github.com>
2020-12-08 14:50:52 +0300

    inter
    inter

--- task1.c
+++ task1.c
@@ -10,7 +10,44 @@
 
 int main(int argc, char *argv[])
 {
-    
+    pid_t pid, p;
+    int i, j, fd1, stat;
+    int fd[2];
+
+    //fprintf(stderr, "%d", argc);
+    if (argc < 5)
+    {   fprintf(stderr, "Error!");    return(1);  }
+
+    pipe(fd);
+    if (fork() == 0)
+    {
+        dup2(fd[1], 1);
+        close(fd[1]);   close(fd[0]);
+        execlp(argv[1], argv[1], NULL);
+        fprintf(stderr, "Error2!"); 
+        exit(1);
+    }
+    if (fork() == 0)
+    {
+        dup2(fd[0], 0);
+        close(fd[1]);   close(fd[0]);
+        execlp(argv[2], argv[2], NULL);
+        fprintf(stderr, "Error3!"); 
+        exit(1);
+    }
+    close(fd[1]);   close(fd[0]);
+    while(wait(&stat) != -1) ;
+
+    if (fork() == 0)
+    {
+        fd1 = open(argv[4], O_WRONLY|O_TRUNC|O_CREAT, 0777);
+        dup2(fd1, 1);   close(fd1);
+        execlp(argv[3], argv[3], NULL);
+        fprintf(stderr, "Error4!"); 
+        exit(1);
+    }
+    while(wait(&stat) != -1) ;
+
 
     return(0);
 }
